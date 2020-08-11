ca4d926abe0bdb01e65b9bc54610c4477273f470
Daniel Lew <danielshlomolew@gmail.com>
2020-08-11 10:56:00 -0400

    updates

--- src/translate-text.js
+++ src/translate-text.js
@@ -1,16 +1,18 @@
 const uuidv4 = require('uuid/v4');
 const config = require('./config').default;
 const querystring = require('querystring');
+console.log(process.env);
+console.log(config);
 
 var key_var = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY';
-if (!config[key_var]) {
-    throw new Error('Please set/export the following environment variable: ' + key_var);
-}
+// if (!config[key_var]) {
+//     throw new Error('Please set/export the following environment variable: ' + key_var);
+// }
 var subscriptionKey = config[key_var];
 var endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT';
-if (!config[endpoint_var]) {
-    throw new Error('Please set/export the following environment variable: ' + endpoint_var);
-}
+// if (!config[endpoint_var]) {
+//     throw new Error('Please set/export the following environment variable: ' + endpoint_var);
+// }
 var endpoint = config[endpoint_var];
 
 const translateMessage = (text, from, to) => {
