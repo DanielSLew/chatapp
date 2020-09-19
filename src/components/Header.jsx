a44343fc02eaeab60a8f7b34ffb6b8d7cb932123
Daniel Lew <danielshlomolew@gmail.com>
2020-09-18 20:04:01 -0400

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,19 +9,37 @@ function Header({ room, placeholder }) {
 
   let displayCopyMessage;
 
-  const copyInviteToClipboard = () => {
+  function writeText(str) {
     clearTimeout(displayCopyMessage);
-    navigator.clipboard.writeText(inviteMessage);
-    setCopySuccess(true);
-    displayCopyMessage = setTimeout(() => setCopySuccess(false), 2000);
-  }
+    return new Promise(function(resolve, reject) {
+      var success = false;
+      function listener(e) {
+        e.clipboardData.setData("text/plain", str);
+        e.preventDefault();
+        success = true;
+      }
+      document.addEventListener("copy", listener);
+      document.execCommand("copy");
+      document.removeEventListener("copy", listener);
+      success ? resolve(): reject();
+      setCopySuccess(true);
+      displayCopyMessage = setTimeout(() => setCopySuccess(false), 2000);
+    });
+  };
+
+  // const copyInviteToClipboard = () => {
+  //   clearTimeout(displayCopyMessage);
+  //   navigator.clipboard.writeText(inviteMessage);
+  //   setCopySuccess(true);
+  //   displayCopyMessage = setTimeout(() => setCopySuccess(false), 2000);
+  // }
 
   return(
     <div>
       <p>
         <Button
           className="mx-auto"
-          onClick={copyInviteToClipboard}
+          onClick={writeText(inviteMessage)}
           style={{ marginRight: '20px' }}
           variant="outline-info"
         >{placeholder}</Button>
